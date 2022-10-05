6869aa9652b8e81a5bd9112e351cb7de5c1d6e76
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-10-05 16:47:54 -0700

    complete
    complete

--- error-handling.js
+++ error-handling.js
@@ -1,26 +1,65 @@
 // 1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+
+
+try {
+  if(Array.isArray(array) === true ){
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  } else {
+    throw new TypeError('Type Error: Invalid Input')
   }
-  return sum;
+} catch (e) {
+  console.log(e.message);
+}
 }
 
-let res = sum(null);
-console.log(res);
+
+
+
+// let res = sum(null);
+// console.log(res);
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+
 // Your code here
+
+const sayName = (name) => {
+  try {
+    if(typeof name === 'string' ){
+      console.log(name);
+    }
+    else {
+      throw new TypeError("Invalid name! Must be a string!")
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+
+// sayName("Alex");
+// sayName(1);
+
 
 // 3.
 function greet(greeting) {
-  if (!greeting) {
-    throw new Error("There was no greeting given.");
+  try{
+  if (typeof greeting === 'string') {
+    console.log(greeting);
+  } 
+  else{
+    throw new Error("Hello World!");
   }
-
-  console.log(greeting);
+} catch (e) {
+  console.log(e.message);
 }
+}
+
+
+greet('Sup foo')
+greet(1)
